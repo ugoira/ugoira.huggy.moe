fac5ba32902074d46e8d7fa00ac54f7586dd2110
huggy <i@huggy.moe>
2021-07-30 16:12:28 +0800

    auto update copyright year

--- src/components/copyright/index.js
+++ src/components/copyright/index.js
@@ -1,5 +1,11 @@
 import Tpl from './template.eft'
 
 export default class Copyright extends Tpl {
-    
+    constructor(copyright) {
+		super({
+			$data: {
+                year: new Date().getFullYear()
+            }
+		})
+	}
 }
