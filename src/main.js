6add668386f018191986c02f036d1e7febb54a21
huggy <i@huggy.moe>
2021-07-31 22:09:14 +0800

    some optimizations & add robots.txt 1. web click to play 2. add webStatus variable 3. convertStatus => convertButtonText

--- src/main.js
+++ src/main.js
@@ -36,6 +36,15 @@ app.description = [
 let illustData = []
 let illustIdsData = []
 let lastInput = null
+
+// webStatus n => normal 
+//			 p => show play button
+//			 d => downloading
+//			 c => converting
+let webStatus = 'n'
+
+let lastY = 0
+
 let videoPlayers = [
 	new IllustVideo(),
 	new IllustVideo(),
@@ -64,13 +73,11 @@ const videoLoader = (sid, count = 4) => {
 				app.illust[i].video.$data.id = illust.id
 			}
 		} else {
-			if (!videoPlayerLoaders[i]) {
-				illustData[i].width = `${document.getElementById(`s${i}`).clientWidth}px`
-				illustData[i].height = `${document.getElementById(`s${i}`).clientHeight}px`
-				videoPlayerLoaders[i] = new IllustVideoLoader({
-					loaderStyle: `width: ${illustData[i].width};height: ${illustData[i].height};`
-				})
-			}
+			illustData[i].width = `${document.getElementById(`s${i}`).clientWidth}px`
+			illustData[i].height = `${document.getElementById(`s${i}`).clientHeight}px`
+			videoPlayerLoaders[i] = new IllustVideoLoader({
+				loaderStyle: `width: ${illustData[i].width};height: ${illustData[i].height};`
+			})
 			app.illust[i].video = videoPlayerLoaders[i]
 		}
 	})
@@ -84,30 +91,32 @@ const play = () => {
 				videosDom[i].play()
 			}
 		}
+		webStatus = 'n'
+		app.$data.convertButtonText = 'Convert'
 	} catch (error) {
 
 	}
 }
 const getIllusts = async ({ state, value }) => {
-	if (app.description[0].$data.description.includes('Downloading')) {
+	if (webStatus == 'd') {
 		return false
 	}
 	if (value.includes('keyflag')) {
 		app.$data.input += ' '
 		app.$data.input += '\n'
 	}
+	if(webStatus == 'p'){
+		return
+	}
 	if (value.length < 6) {
 		app.description[0].$data.description = 'Invalid input'
 		return false
 	}
-	if (app.$data.convertStatus == 'Play') {
-		play()
-		app.$data.convertStatus = 'Convert'
-		return
-	} else if (state || (app.$data.convertStatus !== 'Converting' && app.$data.convertStatus !== 'Play')) {
-		app.description[0].$data.description = ''
+	if (state || (webStatus == 'n')) {
+		webStatus = 'c'
+		app.description[0].$data.description = 'Converting'
 		app.description[1].$data.description = ''
-		app.$data.convertStatus = 'Converting'
+		app.$data.convertButtonText = 'Converting'
 		app.illust = []
 		let data = {
 			data: illustData,
@@ -140,11 +149,9 @@ const getIllusts = async ({ state, value }) => {
 				if (data.ids.length > 9) {
 					app.illust[sid].video = new IllustVideoLoader()
 				}
-				setTimeout(() => {
-					videoLoader(4)
-				}, 300)
-				// load 5 illusts in default
-				// // many browser support it
+				videoLoader(4)
+				// just Firefox full support it
+				// and maybe cost user's a lot of mobile data
 				// let preloadLink = document.createElement('link')
 				// preloadLink.rel = 'preload'
 				// preloadLink.href = illust.url
@@ -159,12 +166,15 @@ const getIllusts = async ({ state, value }) => {
 			}
 		} else {
 			app.description[0].$data.description = 'No pixiv ugoira link found'
+			app.description[1].$data.description = 'Make sure pixiv link\'s type is ugoira (動いら)'
 		}
-		app.$data.convertStatus = 'Convert'
+		webStatus = 'n'
+		app.$data.convertButtonText = 'Convert'
 	}
 	if (!state) {
+		webStatus = 'p'
 		app.description[1].$data.description = 'If the video does not play automatically, please click the Play button.'
-		app.$data.convertStatus = 'Play'
+		app.$data.convertButtonText = 'Play'
 	}
 }
 const download_file = async (url, tryTime = 0) => {
@@ -181,10 +191,61 @@ const download_file = async (url, tryTime = 0) => {
 		return await download_file(url, tryTime + 1)
 	}
 }
-let last_Y = 0
+
+const downloadAllIllusts = async () => {
+	if (webStatus == 'd') {
+		return true
+	}
+	webStatus = 'd'
+	app.description[0].$data.description = 'Downloading files'
+	app.description[1].$data.description = ''
+	let zip = new JSZip()
+
+	// generate readme.html
+	// X S S
+	zip.file('readme.html', `<html><head><meta charset="UTF-8" /><title>Pixiv Ugoira converter</title></head><body><p>This folder is converted & downloaded by <a target="_blank" href="https://ugoira.huggy.moe">ugoira.huggy.moe</a></p><br>illusts link:<br>${illustIdsData.map(u => {
+		let url = `https://www.pixiv.net/artworks/${u}`
+		return `<a target="_blank" href="${url}">${url}</a>`
+	}).join('<br>')}<br><br>You can view this folder's all illusts online on <a href="https://ugoira.huggy.moe/?${illustIdsData.join('-')}">this link</a>`)
+
+	// download *.mp4 in local browser and compress to *.zip
+	await asyncForEach(illustData, async (illust, i) => {
+		app.description[1].$data.description = `${i}/${illustIdsData.length}`
+		let ugoira_data = await download_file(illust.url)
+		if (!ugoira_data) {
+			alert('Some files cannot be downloaded\nYou other try agian or contact me.')
+		}
+		zip.file(`${illust.id}-${illust.title}.mp4`, ugoira_data)
+	})
+	app.description[1].$data.description = 'Compressing'
+	// no need await ? // .then is OK
+	zip.generateAsync({ type: 'blob' }).then(function (content) {
+		saveAs(content, `ugoira.huggy.moe_${new Date().toLocaleString()}.zip`)
+		webStatus = 'n'
+		app.description[0].$data.description = 'Downloaded'
+		app.description[1].$data.description = ''
+	})
+}
+
+// when url with params => input data
+if (location.search !== '') {
+	let hash = location.search.replace('?', '-')
+	app.$data.input = hash.replace(/-/g, '\nhttps://www.pixiv.net/artworks/').replace('\n', '')
+	getIllusts({ value: hash })
+}
+
+const clickPlayEvent = () => {
+	play()
+	document.removeEventListener('click', clickPlayEvent)
+}
+document.addEventListener('click', clickPlayEvent)
+/**
+ * listen scroll event
+ */
 setInterval(() => {
+	// float to integer(string) ~~maybe useful~~
 	let Y = window.scrollY.toString().split('.')[0]
-	if (Y == last_Y) {
+	if (Y == lastY) {
 		return
 	}
 	if (illustIdsData.length > 9) {
@@ -192,54 +253,23 @@ setInterval(() => {
 		for (let i = 0; i < illustsDom.length; i++) {
 			const illustDom = illustsDom[i]
 			if (illustDom.offsetTop > Y) {
-				last_Y = Y
+				lastY = Y
 				videoLoader(i, 4)
 				break
 			}
 		}
 	}
 }, 1000)
-const downloadAllIllusts = async () => {
-	if (app.description[0].$data.description.includes('Downloading')) {
-		return true
-	}
-	app.description[0].$data.description = 'Downloading files'
-	app.description[1].$data.description = ''
-	let zip = new JSZip()
-	// X S S
-	zip.file('readme.html', `<html><head><meta charset="UTF-8" /><title>Pixiv Ugoira converter</title></head><body><p>This folder is converted & downloaded by <a target="_blank" href="https://ugoira.huggy.moe">ugoira.huggy.moe</a></p><br>illusts link:<br>${illustIdsData.map(u => {
-		let url = `https://www.pixiv.net/artworks/${u}`
-		return `<a target="_blank" href="${url}">${url}</a>`
-	}).join('<br>')}<br><br>You can view this folder's all illusts online on <a href="https://ugoira.huggy.moe/?${illustIdsData.join('-')}">this link</a>`)
-	await asyncForEach(illustData, async (illust, i) => {
-		app.description[1].$data.description = `${i}/${illustIdsData.length}`
-		let ugoira_data = await download_file(illust.url)
-		if (!ugoira_data) {
-			alert('Some files cannot be downloaded')
-		}
-		zip.file(`${illust.id}-${illust.title}.mp4`, ugoira_data)
-	})
-	zip.generateAsync({ type: 'blob' }).then(function (content) {
-		saveAs(content, `ugoira.huggy.moe_${new Date().toLocaleString()}.zip`);
-		app.description[0].$data.description = 'Downloaded'
-		app.description[1].$data.description = ''
-	})
-}
 
-
-if (location.search !== '') {
-	let hash = location.search.replace('?', '-')
-	app.$data.input = hash.replace(/-/g, '\nhttps://www.pixiv.net/artworks/').replace('\n', '')
-	// due to autoplay limit, need user click web content
-	getIllusts({ value: hash })
-}
 app.$methods.getIllusts = getIllusts
 app.$methods.downloadAllIllusts = downloadAllIllusts
+
 // Mount app to document
 app.$mount({ target: document.body })
 
 // Resume rendering
 exec()
+
 async function asyncForEach(array, callback) {
 	for (let index = 0; index < array.length; index++) {
 		await callback(array[index], index, array)
@@ -256,7 +286,7 @@ function saveAs(blob, filename) {
 		var elem = window.document.createElement('a')
 		elem.href = window.URL.createObjectURL(blob)
 		elem.download = filename
-		elem.style = 'display:none;opacity:0;color:transparent;';
+		elem.style = 'display:none;opacity:0;color:transparent;'
 		(document.body || document.documentElement).appendChild(elem)
 		if (typeof elem.click === 'function') {
 			elem.click()
